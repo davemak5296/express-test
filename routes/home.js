a3e09d930365e3d73cccea21123d4002394247d3
Dave Mak <mak9652615@gmail.com>
2022-03-09 17:59:05 +0800

    insert cm and display cm OK

--- routes/home.js
+++ routes/home.js
@@ -12,12 +12,30 @@ const connection = mysql.createConnection({
 
 router.get('/', function (req, res, next) {
     if ( req.session.loggedin ) {
-        res.render('index', {
-            nickname: req.session.nickname
+        let errMsg = req.session.errMsg;
+        let sucMsg = req.session.sucMsg;
+        connection.query({
+            sql: 'SELECT C.id, C.username, U.nickname, C.content, C.created_at, C.is_hide FROM comments as C LEFT JOIN users as U ON C.username = U.username WHERE is_hide = 0 ORDER BY C.created_at DESC'
+        }, function (error, results, fields) {
+            if (error) {
+                throw error;
+            }
+            for(let i = 0; i < results.length; i++) {
+                console.log(results[i]);
+            }
+            req.session.errMsg = undefined;
+            req.session.sucMsg = undefined;
+            res.render('index', {
+                nickname: req.session.nickname,
+                results: results,
+                errMsg: errMsg,
+                sucMsg: sucMsg
+            })
         })
     } else {
         res.redirect('/');
     }
 })
+
 
 module.exports = router;
--- routes/index.js
+++ routes/index.js
@@ -1,5 +1,14 @@
 var express = require('express');
 var router = express.Router();
+const session = require('express-session');
+var mysql = require('mysql');
+
+const connection = mysql.createConnection({
+  host : 'localhost',
+  user : 'Dave',
+  password : '1996',
+  database : 'msg_board_express'
+});
 
 router.get('/', function (req, res, next) {
     if ( req.session.loggedin ) {
@@ -8,5 +17,28 @@ router.get('/', function (req, res, next) {
         res.render('index', { title: '留言板' });
     }
 });
+
+router.post('/comment', function (req, res, next) {
+    if ( req.session.loggedin ) {
+        if ( req.body.content !== "" ) {
+            req.session.content = req.body.content;
+            connection.query({
+                sql: 'INSERT INTO comments(username, content) VALUES(?,?)',
+                values: [
+                    req.session.username,
+                    req.session.content
+                ]
+            }, function (error, results, fields) {
+                req.session.sucMsg = 'Comment saved.';
+                res.redirect('/home');
+            })
+        } else {
+            req.session.errMsg = 'Please write your comments.';
+            res.redirect('/home');
+        }
+    } else {
+        res.redirect('/');
+    }
+})
 
 module.exports = router;
